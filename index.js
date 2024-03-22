d56c8c6209ac395ab08424daa3f98cec8593169d
Abhishek Gupta <bsaagupta77@gmail.com>
2024-03-22 19:02:18 +0530

    added codes

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const { log } = require('console');
+
 const express = require('express');
 const http = require('http');
 const socketio = require('socket.io');
+const connect = require('./config/database-config');
 
 const app = express();
 const server = http.createServer(app);
@@ -9,21 +10,33 @@ const io = socketio(server);
 
 io.on('connection', (socket) => {
     console.log(' a user connected');
-
+    socket.on('join_room', (roomid) => {
+        socket.join(roomid);
+    })
     socket.on('msg_send',(data)=>{
         console.log(data);
-        // io.emit('msg_rcvd', data) //-->give message to all the clinet connected to server
+        io.to(data.roomid).emit('msg_rcvd', data) //-->give message to all the clinet connected to server
         //socket.emit('msg_rcvd', data)//--> give message to same client the one who send the message
-        socket.broadcast.emit('msg_rcvd', data) //-->except the one who send message it will give message to all client
+        // socket.broadcast.emit('msg_rcvd', data) //-->except the one who send message it will give message to all client
     })
   
 });
 
+app.set('view engine', 'ejs');
 
-app.use('/', express.static(__dirname+'/public'));
+app.use('/', express.static(__dirname + '/public'));
 
-server.listen(3000, ()=> {
+app.get('/chat/:roomId', (req,res) => {
+    res.render('index',{
+        name: 'Abhishek',
+        id: req.params.roomId
+    });
+});
+
+server.listen(3000, async ()=> {
     
   console.log('Server started');
+  await connect();
+  console.log('MongoDB Connected');
 
 });
